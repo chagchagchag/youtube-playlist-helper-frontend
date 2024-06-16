c9e870f34aecc4add5b799e1c1d82a469ea2c995
chagchagchag <chagchagchag.dev@gmail.com>
2024-06-16 21:34:07 +0900

    login with google

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function loginWighGoogle(){
+  const clientId = "580193018098-65pgl90826fmd31chpbb9areco0mn6hs.apps.googleusercontent.com"
+  const redirect_uri = "http://localhost:8080/welcome"
+  const scope = "profile"
+  const response_type="code"
+  
+  const url = 'https://accounts.google.com/o/oauth2/v2/auth?'
+  +'client_id='+clientId
+  +'&redirect_uri='+redirect_uri
+  +'&response_type='+response_type
+  +'&scope='+scope
+  
+  console.log('welcome')
+
+  showSocialLoginPopup(url)
+}
+
+function showSocialLoginPopup(url){
+  const popupHeight = '500'
+  const popupWidth = '500'
+  let popupOptions = 'height=--popupHeight--,width=--popupWidth--,left=--popupX--,top=--popupY--,scrollbars=yes,resizable=yes'
+
+  popupOptions = popupOptions.replace('--popupHeight--', popupHeight)
+  popupOptions = popupOptions.replace('--popupWidth--', popupWidth)
+
+  openPopup(url, popupOptions)
+  return false
+}
+
+function openPopup(url, options){
+  window.open(
+    url,
+    '_blank',
+    options
+  )
+}
+
+function logout(){
+  // remove from localStorage or cookie 
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <img alt="" src="image/google/web_light_rd_SI@2x.png" onClick={loginWighGoogle}/>
     </div>
   );
 }
